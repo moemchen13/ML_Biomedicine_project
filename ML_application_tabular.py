59d4656c543a708f28b32d814e0cecab0de324d5
moemchen13 <super.moritz.christ@gmx.de>
2024-09-09 11:44:54 +0200

    Added task selction

--- ML_application_tabular.py
+++ ML_application_tabular.py
@@ -4,28 +4,40 @@ import backend_ML as be
 st.set_page_config(page_title="ML Tabular data site", layout="wide")
 st.title("Welcome to the your online Machine learning side")
 
-data_uploader,conf_uploader = st.columns([0.9,0.1])
+
+data_uploader, conf_uploader = st.columns([0.8,0.2],vertical_alignment="center")
 with data_uploader:
     f = st.file_uploader("Please upload the dataset you want to analyse with Machine learning models")
+    if f:
+        df = be.return_df(f)
+        st.success("File uploaded")
+        df = st.data_editor(df)
+
 with conf_uploader:
-    conf = st.file_uploader(label="Config",label_visibility="collapsed")
-if f:
-    df = be.return_df(f)
-    st.success("File uploaded")
-    df = st.data_editor(df)
+    st.write("")
+    with st.popover("Upload \n config"):
+        conf = st.file_uploader("config json")
+        if conf:
+            config = be.return_df(conf)
+            st.success("Uploaded config file")
+            
 
 
 
+st.markdown("Select task")
+clas,tog,reg = st.columns(3)
 
-popover =  st.popover("Select task")
-
-reg,clas = st.columns(2)
-with reg:
-        st.markdown("Classification")
+with tog:
+        task_is_regression = st.toggle("")
 with clas:
-        red = popover.toggle("Use Regression")
+    if task_is_regression:
+        st.markdown("Classification")
+    else:
+        st.markdown(":blue[Classification]")
+with reg:
+    if task_is_regression:
+        st.markdown(":red[Regression]")
+    else:
+        st.markdown("Regression")
 
-if red:
-    st.write(":red[We now use regresseion]")
-else:
-    st.write("We use classification")
+
