08c3249609e2787e86aa6322e91aa2d9aeb184bf
moemchen13 <super.moritz.christ@gmx.de>
2024-09-13 15:03:28 +0200

    changes

--- Hello.py
+++ Hello.py
@@ -7,12 +7,19 @@ st.title("Main Page - Navigation")
 col1, col2, col3 = st.columns(3)
 with col2:
     st.header("Our Team")
-    st.image("./data/pics/i_me.jpg")
+    st.image("./data/pics/jonathan_moritz.jpg")
 st.write("Team:")
 st.markdown("<p>Moritz Christ<br> Master Machine Learning (3. Semester)<br>Jonathan Nemitz<br>Master Machine Learning (5. Semester)</p>",unsafe_allow_html=True)
 
 
+
+st.write("<h4>Conclusions</h4>")
+st.markdown("<ul><li>We never will be webdesigners</li> <li>streamlit is nice for demos</li> <li>streamlit is buggy</li> <li>Look up known issues with the cloud (cv2)</li></ul>",unsafe_allow_html=True)
+
+
+
+
 st.write("<h4>Impressum</h4>")
-st.markdown("Eberhard Karls Universität Tübingen <br> Project in the framework of the ML for Biomedicine internship<br><br> Special thanks to: <br>Elisa Maske<br>Shirin Davis <br>Nintendo",unsafe_allow_html=True)
+st.markdown("Eberhard Karls Universität Tübingen <br> Project in the framework of the ML for Biomedicine internship<br><br> <h5>Special thanks to:</h5> <ol><li>Elisa Maske</li><li>Nintendo</li><li>Shirin Davis</li> <ol>",unsafe_allow_html=True)
 
 
